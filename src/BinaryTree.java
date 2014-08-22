1e1fe99678e957403d735b12ecfa60c6e970f4f4
MrFaton <firefly90@inbox.ru>
2014-08-22 18:51:37 +0300

    update from work

--- src/BinaryTree.java
+++ src/BinaryTree.java
@@ -2,38 +2,39 @@
  * Created by root on 18.08.2014.
  */
 public class BinaryTree {
-    public static TreeNode root=new TreeNode(5, null, null);
-    static int i=0;
+    public static TreeNode root = new TreeNode(5, null, null);
+    static int i = 0;
+
     public static void main(String[] args) {
-        makeTree(6,7,4,2,3,1);
-        i=0;
-        System.out.println("Колличество всех элементов в дереве = "+size(root));
-        i=0;
-        System.out.println("Сумма всех элементов дерева = "+sum(root));
-        i=0;
-        System.out.println("Максимальное значение среди всех элементов в дереве = "+max(root));
-        i=0;
-        System.out.println("Максимальная высота дерева = "+height(root));
+        makeTree(6, 7, 4, 2, 3, 1);
+        i = 0;
+        System.out.println("Колличество всех элементов в дереве = " + size(root));
+        i = 0;
+        System.out.println("Сумма всех элементов дерева = " + sum(root));
+        i = 0;
+        System.out.println("Максимальное значение среди всех элементов в дереве = " + max(root));
+        i = 0;
+        System.out.println("Максимальная высота дерева = " + height(root));
         showTree(root);
     }
 
-    public static TreeNode makeTree(int... values){
-        for (int i=values.length-1; i>=0; i--){
+    public static TreeNode makeTree(int... values) {
+        for (int i = values.length - 1; i >= 0; i--) {
             inseart(values[i], root);
         }
         return root;
     }
 
-    public static TreeNode inseart(int value, TreeNode nodeList){
-        if (value<nodeList.value){
-            if (nodeList.left==null){
-                nodeList.left=new TreeNode(value, null, null);
+    public static TreeNode inseart(int value, TreeNode nodeList) {
+        if (value < nodeList.value) {
+            if (nodeList.left == null) {
+                nodeList.left = new TreeNode(value, null, null);
             } else {
                 inseart(value, nodeList.left);
             }
         } else {
-            if(nodeList.right==null){
-                nodeList.right=new TreeNode(value, null,null);
+            if (nodeList.right == null) {
+                nodeList.right = new TreeNode(value, null, null);
             } else {
                 inseart(value, nodeList.right);
             }
@@ -41,28 +42,28 @@ public class BinaryTree {
         return nodeList;
     }
 
-    public static Integer size(TreeNode list){
-        if(list!=null){
+    public static Integer size(TreeNode list) {
+        if (list != null) {
             i++;
             size(list.left);
             size(list.right);
         }
-        return  i;
+        return i;
     }
 
-    public static Integer sum(TreeNode list){
-        if (list!=null){
-            i+=list.value;
+    public static Integer sum(TreeNode list) {
+        if (list != null) {
+            i += list.value;
             sum(list.left);
             sum(list.right);
         }
         return i;
     }
 
-    public static Integer max(TreeNode list){
-        if(list!=null){
-            if (i<list.value){
-                i=list.value;
+    public static Integer max(TreeNode list) {
+        if (list != null) {
+            if (i < list.value) {
+                i = list.value;
             }
             max(list.left);
             max(list.right);
@@ -70,15 +71,16 @@ public class BinaryTree {
         return i;
     }
 
-    public static Integer height(TreeNode list){
-        
+    public static Integer height(TreeNode list) {
+        System.out.println("надо почитинь");
+        return 0;
     }
 
-    public static String showTree(TreeNode tree){
-        if(tree!=null){
-            String left=(tree.left==null ? "нет" : ""+tree.left.value);
-            String right=(tree.right==null ? "нет" : ""+tree.right.value);
-            System.out.println("Корень: "+tree.value+", левая ветка: "+left+", правая ветка: "+right);
+    public static String showTree(TreeNode tree) {
+        if (tree != null) {
+            String left = (tree.left == null ? "нет" : "" + tree.left.value);
+            String right = (tree.right == null ? "нет" : "" + tree.right.value);
+            System.out.println("Корень: " + tree.value + ", левая ветка: " + left + ", правая ветка: " + right);
             showTree(tree.left);
             showTree(tree.right);
         }
